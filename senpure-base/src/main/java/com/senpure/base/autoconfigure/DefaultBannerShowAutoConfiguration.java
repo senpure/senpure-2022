99ff7dd629c23b4f34b7875b2f029d0d30c30842
senpure <senpure@senpure.com>
2020-06-18 14:33:10 +0800

    normal

--- senpure-base/src/main/java/com/senpure/base/autoconfigure/DefaultBannerShowAutoConfiguration.java
+++ senpure-base/src/main/java/com/senpure/base/autoconfigure/DefaultBannerShowAutoConfiguration.java
@@ -1,37 +1,166 @@
 package com.senpure.base.autoconfigure;
 
+import com.senpure.base.AppEvn;
 import com.senpure.base.util.BannerShow;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 
-
+@EnableConfigurationProperties(DefaultBannerShowAutoConfiguration.BannerProperties.class)
 public class DefaultBannerShowAutoConfiguration {
 
-    @Value("${senpure.default.banner.enable:true}")
-    private boolean enable = true;
-
-    public void setEnable(boolean enable) {
-        this.enable = enable;
-    }
+    //是否主动设置了减半显示
+    private static boolean activeHalf;
 
     @Bean
     @Order
-    public DefaultBannerShow defaultBannerShow() {
-        return new DefaultBannerShow();
+    public DefaultBannerShow defaultBannerShow(BannerProperties bannerProperties) {
+        return new DefaultBannerShow(bannerProperties);
     }
 
-    class DefaultBannerShow implements ApplicationRunner {
+    static class DefaultBannerShow implements ApplicationRunner {
+        private final BannerProperties bannerProperties;
+
+        public DefaultBannerShow(BannerProperties bannerProperties) {
+            this.bannerProperties = bannerProperties;
+        }
 
         @Override
         public void run(ApplicationArguments applicationArguments) throws Exception {
-            if (enable) {
+            if (bannerProperties.isEnable()) {
+                if (bannerProperties.isTableStyle()) {
+                    BannerShow.setDownLeft("╚");
+                    BannerShow.setDownRight("╝");
+                    BannerShow.setTopLeft("╔");
+                    BannerShow.setTopRight("╗");
+                    BannerShow.setTopDown("═");
+                    BannerShow.setLeftRight("║");
+                    if (activeHalf) {
+                        BannerShow.setTopDownHalf(bannerProperties.isTopDownHalf());
+                    } else {
+                        if (AppEvn.isWindowsOS()) {
+                            BannerShow.setTopDownHalf(true);
+                        }
+                    }
+                } else {
+                    BannerShow.setDownLeft(bannerProperties.getDownLeft());
+                    BannerShow.setDownRight(bannerProperties.getDownRight());
+                    BannerShow.setTopLeft(bannerProperties.getTopLeft());
+                    BannerShow.setTopDown(bannerProperties.getTopDown());
+                    BannerShow.setTopDown(bannerProperties.getTopDown());
+                    BannerShow.setLeftRight(bannerProperties.getLeftRight());
+                    BannerShow.setTopDownHalf(bannerProperties.isTopDownHalf());
+                }
+
                 BannerShow.show();
                 Thread.sleep(1500);
             }
 
         }
+    }
+
+    @ConfigurationProperties(prefix = "senpure.default.banner")
+    static
+    class BannerProperties {
+        /**
+         * spring启动完成之后是否打印banner
+         */
+        private boolean enable = true;
+        /**
+         * 使用制表符风格，其他的字符配置将会失效
+         */
+        private boolean tableStyle;
+        private String topLeft = "*";
+        private String topRight = "*";
+        private String downLeft = "*";
+        private String downRight = "*";
+        private String topDown = "*";
+        /**
+         * 宽字符不同的控制台有不同的表现形式，是否减半显示
+         */
+        private boolean topDownHalf;
+        private String leftRight = "*";
+
+
+        public BannerProperties() {
+        }
+
+
+        public void setTopDownHalf(boolean topDownHalf) {
+            this.topDownHalf = topDownHalf;
+            activeHalf = true;
+        }
+
+        public boolean isEnable() {
+            return enable;
+        }
+
+        public void setEnable(boolean enable) {
+            this.enable = enable;
+        }
+
+        public String getTopLeft() {
+            return topLeft;
+        }
+
+        public void setTopLeft(String topLeft) {
+            this.topLeft = topLeft;
+        }
+
+        public String getTopRight() {
+            return topRight;
+        }
+
+        public void setTopRight(String topRight) {
+            this.topRight = topRight;
+        }
+
+        public String getDownLeft() {
+            return downLeft;
+        }
+
+        public void setDownLeft(String downLeft) {
+            this.downLeft = downLeft;
+        }
+
+        public String getDownRight() {
+            return downRight;
+        }
+
+        public void setDownRight(String downRight) {
+            this.downRight = downRight;
+        }
+
+        public String getTopDown() {
+            return topDown;
+        }
+
+        public void setTopDown(String topDown) {
+            this.topDown = topDown;
+        }
+
+        public String getLeftRight() {
+            return leftRight;
+        }
+
+        public void setLeftRight(String leftRight) {
+            this.leftRight = leftRight;
+        }
+
+        public boolean isTopDownHalf() {
+            return topDownHalf;
+        }
+
+        public boolean isTableStyle() {
+            return tableStyle;
+        }
+
+        public void setTableStyle(boolean tableStyle) {
+            this.tableStyle = tableStyle;
+        }
+
     }
 }
--- senpure-base/src/main/java/com/senpure/base/util/BannerShow.java
+++ senpure-base/src/main/java/com/senpure/base/util/BannerShow.java
@@ -11,8 +11,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 public class BannerShow {
@@ -34,6 +36,16 @@ public class BannerShow {
         strs.add("     \\__\\/         \\__\\/         \\__\\/         \\__\\/         \\__\\/         \\__\\/         \\__\\/");
     }
 
+    //控制台对于不同的字体宽字符有不同的表现，边框修饰默认使用单字符
+    private static String topLeft = "*";
+    private static String topRight = "*";
+    private static String downLeft = "*";
+    private static String downRight = "*";
+    private static String topDown = "*";
+    private static String leftRight = "*";
+
+    private static boolean topDownHalf;
+
     public static void show() {
         //LOGGER.debug("\n\n{}", str());
         System.out.println(str());
@@ -44,7 +56,7 @@ public class BannerShow {
         List<String> strs = null;
         try {
             InputStream
-                    inputStream = BannerShow.class.getClassLoader().getResourceAsStream("showbanner.txt");
+                    inputStream = BannerShow.class.getClassLoader().getResourceAsStream("show-banner.txt");
             // 建立一个输入流对象reader
             if (inputStream != null) {
                 InputStreamReader reader = new InputStreamReader(
@@ -60,7 +72,7 @@ public class BannerShow {
                 inputStream.close();
             }
 
-        } catch (IOException e) {
+        } catch (IOException ignored) {
 
         }
         if (strs == null || strs.size() == 0) {
@@ -69,52 +81,62 @@ public class BannerShow {
         int length = 0;
         for (int i = 0; i < strs.size(); i++) {
             int tl = strs.get(i).length();
-            length = tl > length ? tl : length;
+            length = Math.max(tl, length);
         }
         if (length % 2 != 0) {
             length++;
         }
 
         StringBuilder banner = new StringBuilder();
-        String top = "";
-        if (AppEvn.isWindowsOS()) {
-            top = getStr("═", length >> 1);
-        } else {
-            top = getStr("═", length);
-        }
-        top = "╔" + top + "╗";
-        top = AnsiOutput.toString(new Object[]{AnsiColor.BRIGHT_RED, top});
+        String top = getStr(length);
+        // top = "╔" + top + "╗";
+        //top = "*" + top + "*";
+        top = topLeft + top + topRight;
+        top = AnsiOutput.toString(AnsiColor.BRIGHT_RED, top);
         banner.append(top);
         banner.append("\n");
 
         for (int i = 0; i < strs.size(); i++) {
-            banner.append(AnsiOutput.toString(new Object[]{AnsiColor.BRIGHT_RED, "║"}));
-            banner.append(AnsiOutput.toString(new Object[]{AnsiColor.BRIGHT_CYAN, strs.get(i)}));
+            // banner.append(AnsiOutput.toString(AnsiColor.BRIGHT_RED, "║"));
+            // banner.append(AnsiOutput.toString(AnsiColor.BRIGHT_RED, "*"));
+            banner.append(AnsiOutput.toString(AnsiColor.BRIGHT_RED, leftRight));
+            banner.append(AnsiOutput.toString(AnsiColor.BRIGHT_CYAN, strs.get(i)));
             int l = strs.get(i).length();
             String temp = getStr(" ", length - l);
-            banner.append(AnsiOutput.toString(new Object[]{AnsiColor.BRIGHT_RED, temp + "║"}));
+            banner.append(AnsiOutput.toString(AnsiColor.BRIGHT_RED, temp + leftRight));
+            //  banner.append(AnsiOutput.toString(AnsiColor.BRIGHT_RED, temp + "*"));
             banner.append("\n");
         }
-        String empty;
-        empty = getStr(" ", length);
-        empty = "║" + empty + "║";
-        empty = AnsiOutput.toString(new Object[]{AnsiColor.BRIGHT_RED, empty});
-        banner.append(empty);
-        banner.append("\n");
-        String down = "";
-        if (AppEvn.isWindowsOS()) {
-            down = getStr("═", length >> 1);
-        } else {
-            down = getStr("═", length);
-        }
-        down = "╚" + down + "╝";
-        down = AnsiOutput.toString(new Object[]{AnsiColor.BRIGHT_RED, down});
+        //  String empty;
+        // empty = getStr(" ", length);
+        //  empty = "║" + empty + "║";
+        // empty = "*" + empty + "*";
+        // empty = AnsiOutput.toString(AnsiColor.BRIGHT_RED, empty);
+        // banner.append(empty);
+        // banner.append("\n");
+        String down = getStr( length);
+
+        // down = "╚" + down + "╝";
+        //down = "*" + down + "*";
+        down = downLeft + down + downRight;
+        down = AnsiOutput.toString(AnsiColor.BRIGHT_RED, down);
         banner.append(down);
         banner.append("\n");
         return banner.toString();
     }
 
+    private static String getStr(int num) {
+        //LOGGER.info("ch {} num {}", ch, num);
+
+        if (topDownHalf) {
+            return getStr(topDown, num >> 1);
+        } else {
+            return getStr(topDown, num);
+        }
+    }
+
     private static String getStr(String ch, int num) {
+        //LOGGER.info("ch {} num {}", ch, num);
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < num; i++) {
             sb.append(ch);
@@ -124,7 +146,7 @@ public class BannerShow {
     }
 
     public static void bannerText(String name) {
-        List<String> strs = new ArrayList<>();
+
 
         try {
             InputStream inputStream;
@@ -133,26 +155,83 @@ public class BannerShow {
             InputStreamReader reader = new InputStreamReader(
                     inputStream);
             BufferedReader br = new BufferedReader(reader);
-            strs = new ArrayList<>();
             while (br.ready()) {
                 String line = br.readLine();
-                // strs.add(line);
+
                 line = line.replace("\\", "\\\\");
                 System.out.println("strs.add(\"" + line + "\") ;");
             }
             br.close();
             reader.close();
             inputStream.close();
-        } catch (IOException e) {
+        } catch (IOException ignored) {
 
         }
     }
 
+
+    public static String getTopLeft() {
+        return topLeft;
+    }
+
+    public static void setTopLeft(String topLeft) {
+        BannerShow.topLeft = topLeft;
+    }
+
+    public static String getTopRight() {
+        return topRight;
+    }
+
+    public static void setTopRight(String topRight) {
+        BannerShow.topRight = topRight;
+    }
+
+    public static String getDownLeft() {
+        return downLeft;
+    }
+
+    public static void setDownLeft(String downLeft) {
+        BannerShow.downLeft = downLeft;
+    }
+
+    public static String getDownRight() {
+        return downRight;
+    }
+
+    public static void setDownRight(String downRight) {
+        BannerShow.downRight = downRight;
+    }
+
+    public static String getTopDown() {
+        return topDown;
+    }
+
+    public static void setTopDown(String topDown) {
+        BannerShow.topDown = topDown;
+    }
+
+    public static String getLeftRight() {
+        return leftRight;
+    }
+
+    public static void setLeftRight(String leftRight) {
+        BannerShow.leftRight = leftRight;
+    }
+
+    public static boolean isTopDownHalf() {
+        return topDownHalf;
+    }
+
+    public static void setTopDownHalf(boolean topDownHalf) {
+        BannerShow.topDownHalf = topDownHalf;
+    }
+
     public static void main(String[] args) {
-        System.setProperty("PID", AppEvn.getPid());
+        System.out.println(Charset.defaultCharset());
+        System.setProperty("PID", Objects.requireNonNull(AppEvn.getPid()));
         AnsiOutput.setEnabled(AnsiOutput.Enabled.ALWAYS);
 
-        bannerText("showbanner.txt");
+        bannerText("show-banner.txt");
 
         show();
     }
